b12fad3f243f6cf6627f51101d25ff78d75e6d19
Azurlin <Azurlin@outlook.com>
2019-11-26 17:54:41 +0800

    11.26

--- pages/userindex/userindex.js
+++ pages/userindex/userindex.js
@@ -13,48 +13,73 @@ Page({
       content: '3',
       read: "未读",
     }),
+
   data: {
      i :0,
     container: null,
     active: 0,
     obj: [],  
-    notice:({
-      title:"新的通知",
-      content: '通知内容1',
-      read:"未读",
-    },
-      {
-        title: "新的通知",
-        content: '通知内容2',
-        read: "未读",
-      },
-      {
-        title: "新的通知",
-        content: '通知内容3',
-        read: "未读",
-      })
+    /*notice:({
+      publisher_id:"", //发布者id
+      title: '', //通知标题
+      description:'', //通知内容
+      start_date:'', //开始时间
+      end_date:'', //截止时间
+      attachments_id:'', // 附件地址
+      submit_attachments:'' //附件标记
+    })*/
+    notice:({})
+
   },
 
   
   /***增加组件 */
   onTapAdd: function (e) {
-    console.log(this.ar[1]);
-var i =0;
-
+    //console.log(this.ar[1]);
+    //var i =0;
+    var stro = this.analysisRes(); //接受解析完返回的数组
+    console.log("stro:"+stro);
+    this.setData({
+      notice:stro
+    })
+    //console.log(typeof(this.data.notice));
+    console.log("notice:"+this.data.notice);
+    this.updateTap();
+    console.log("后notice:"+this.data.notice);
+  },
+  updateTap:function(){
     var temp = this.data.obj;
     temp.push(this.data.notice);
     this.setData({
       obj: temp
     })
+    //console.log("obj[0]:99999"+this.data.obj[0]);
   },
-  /***** 删除最后一个组件，也可以修改删除指定组件*/
-  onTapDel: function (e) {
-   
-    var temp = this.data.obj;
-    temp.pop(this.data.obj);
+  /***** 删除组件，也可以修改删除指定组件*/  // temp[0] = notic
+  onTapDel: function (e) { 
+    //var temp = this.data.notice;
+    //console.log(temp[0]);
+    //console.log(this.data.notice);
+    //temp.pop(this.data.obj[0][0]);
+    this.data.obj[0].splice(0,1);
+    console.log("obj[0]:"+this.data.obj[0]);
+    //temp.splice(0,1);
+    //console.log("temp:"+temp);
+    //this.data.notice.splice(1);
+    //console.log(this.data.notice);
+    //console.log(this.data.obj[0]);
     this.setData({
+      notice:this.data.obj[0]
+    })
+    console.log("updateTap前的notice:"+this.data.notice);
+    this.updateTap();
+    //console.log(temp);
+    console.log("updateTap之后的notice:"+this.data.notice);
+    /*this.setData({
       obj: temp
-    })},
+    })*/
+    
+  },
 
   onChange(event) {
     this.setData({ active: event.detail });
@@ -80,7 +105,7 @@ var i =0;
     for (var i = 0; i < str.length; i++) {
       req += '<' + str[i] + '>' + content[i] + '</>';
     }
-    console.log(req);
+    //console.log(req);
     this.test1(req);
 
   },
@@ -106,7 +131,7 @@ var i =0;
 
     wx.onSocketMessage(function (res) {
       this.analysisRes1(res.data)
-      console.log(res)
+      //console.log(res)
       wx.closeSocket({
 
       })
@@ -118,19 +143,29 @@ var i =0;
     })
   },
 
-  analysisRes1: function () { //解析收到的数据
-    var str = '{ "task1": "{"publisher_id": "001", "title": "title1", "description": "description", "start_date": "2019-11 - 25 11: 45: 11", "end_date": "2019-11 - 25 11: 45: 11", "attachments_id": "attachments_id", "submit_attachments": 11}", "task2": "{"publisher_id": "001", "title": "title1", "description": "description", "start_date": "2019-11 - 25 11: 45: 57", "end_date": "2019-11 - 25 11: 45: 57", "attachments_id": "attachments_id", "submit_attachments": 11}", "task3": "{"publisher_id": "001", "title": "title1", "description": "description", "start_date": "2019-11 - 25 11: 47: 08", "end_date": "2019-11 - 25 11: 47: 08", "attachments_id": "attachments_id", "submit_attachments": 11}" }';
+  analysisRes: function () { //解析收到的数据
+    var str = '{ "task1": {"publisher_id": "001", "title": "title1", "description": "description", "start_date": "2019-11 - 25 11: 45: 11", "end_date": "2019-11 - 25 11: 45: 11", "attachments_id": "attachments_id", "submit_attachments": 11}, "task2": {"publisher_id": "001", "title": "title2", "description": "description", "start_date": "2019-11 - 25 11: 45: 57", "end_date": "2019-11 - 25 11: 45: 57", "attachments_id": "attachments_id", "submit_attachments": 11}, "task3": {"publisher_id": "001", "title": "title3", "description": "description", "start_date": "2019-11 - 25 11: 47: 08", "end_date": "2019-11 - 25 11: 47: 08", "attachments_id": "attachments_id", "submit_attachments": 11} }';
 
     var st = '{ "bar": "property", "baz": 3 }';
+    var jsonStr; var jsonObject;
     var jsObject = JSON.parse(str);    //转换为json对象
+    //console.log(jsObject);
+    let myArray = [];
     for (var key in jsObject) {
-      console.log(key); 
-      console.log(jsObject[key]); //json对象的值  
+      //console.log(key);
+      //console.log(jsObject[key]); //json对象的值
+      jsonStr = JSON.stringify(jsObject[key]);
+      jsonObject = JSON.parse(jsonStr);
+      //console.log(jsonObject); //jsonObject {key0:v,key1:v}
+      myArray.push(jsonObject);
 
+      for (var k in jsonObject) {
+        //console.log(jsonObject[k]);
+      }
     }  
-   
+   // console.log(myArray);
 
-
+    return myArray;
   },
 
 
@@ -138,7 +173,7 @@ var i =0;
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.analysisRes1();
+    this.analysisRes();
   },
 
   /**
